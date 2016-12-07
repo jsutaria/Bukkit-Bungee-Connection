6e803cebc9cf68000523f38174cef451f40f2ae8
Jainil Sutaria <jainilsutaria1@gmail.com>
2016-12-07 12:10:58 -0500

    WIP

--- src/main/java/com/jainilsutaria/api/API_Player.java
+++ src/main/java/com/jainilsutaria/api/API_Player.java
@@ -1,5 +1,14 @@
 package com.jainilsutaria.api;
 
 public class API_Player {
+	private String name;
+	
+	public API_Player(String name) {
+		this.name = name;
 
+	}
+	
+	public String getName() {
+		return this.name;
+	}
 }
--- src/main/java/com/jainilsutaria/api/bukkit/BukkitAPI.java
+++ src/main/java/com/jainilsutaria/api/bukkit/BukkitAPI.java
@@ -1,7 +1,33 @@
 package com.jainilsutaria.api.bukkit;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class BukkitAPI extends JavaPlugin {
 
+	private FileConfiguration config; 
+	
+	@Override
+	public void onEnable() {
+		loadDefaults();
+		registerServer();
+		config = getConfig();
+		checkLoad();
+	}
+
+	private void checkLoad() {
+		
+	}
+
+	private void registerServer() {
+
+	}
+	
+	private void loadDefaults() {
+		config.addDefault("server-name", this.getServer().getName());
+		config.addDefault("bungee-ip", "localhost");
+		config.addDefault("connection-port", -1);
+		config.options().copyDefaults(true);
+		saveConfig();
+	}
 }
